5903ff740fc67edb9ffed3f91498a3f8c4b531a8
Audrey Yang <audrey.butter0930@gmail.com>
2023-03-25 14:33:36 -0400

    Setting up files

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Mutual Aid Fund App
-insert project specification here
+A software system that will allow contributors to create, view, and donate to Mutal Aid funds. 
+
+---
+## Documentation Notes
+- **public** is the folder for all the html files - name possibly subjected to change
+
+### Running Java with JSON
+- **Complie**: *javac -cp "json-simple-1.1.1.jar;." [filename].java*
+- **Run**: *java -cp "json-simple-1.1.1.jar;." [filename]*
---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+// This is the '/test' endpoint that you can use to check that this works
+app.use('/test', (req, res) => {
+	// create a JSON object
+	var data = { 'message' : 'It works!' };
+      	// send it back
+	res.json(data);
+    });
+
+// This just sends back a message for any URL path not covered above
+app.use('/', (req, res) => {
+	res.send('Default message.');
+    });
+
+// This starts the web server on port 3000. 
+app.listen(3000, () => {
+	console.log('Listening on port 3000');
+    });
