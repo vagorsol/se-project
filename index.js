a409744741564e7bb39ea2dae82161abf4f2ae4b
JShand18 <jor.shand@gmail.com>
2023-03-27 01:31:17 -0400

    Add new user functionality

--- index.js
+++ index.js
@@ -1,6 +1,194 @@
+// set up Express
 var express = require('express');
 var app = express();
 
+// set up BodyParser
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/* Connecting to MongoDB Atlas
+*/
+var mongoose = require('mongoose');
+mongoose.connect(
+	'mongodb+srv://jshand:SoftTeam@fundcollections.7fyo0g4.mongodb.net/?retryWrites=true&w=majority', 
+	{
+	  useNewUrlParser: true,
+	  useUnifiedTopology: true
+	}
+  ); 
+
+  const db = mongoose.connection;
+  db.on("error", console.error.bind(console, "connection error: "));
+  db.once("open", function () {
+	console.log("Connected successfully");
+  });
+
+// import the Fund class from Fund.js
+var Fund = require('./Fund.js');
+var User = require('./User.js');
+
+/**
+ * Gets the fund with the exact entered name.
+ * Possible future modifications: 
+ *  	- search all bodies of all funds for anything that matches 
+ * 		- have option for "advanced" search (specific fields, etc.)
+ * author @vagorsol 
+ */
+// search: check all bodies of all funds if contains, then return all w "funds with '[entry]' in them"
+app.use('/view', (req, res)=> {
+	var filter = {'name' : req.body.name};
+	console.log(filter);
+	Fund.findOne( filter, (err, fund) => {
+		res.type('html').status(200);
+		console.log(fund);
+		if (err) {
+			console.log('Error: ' + err);
+			res.write(err);
+		} else if (!filter || fund == null) {
+			res.write('No fund with that ID exists!');
+			res.write(" <a href=\"/request\">[Back to Search]</a>");
+		} else {
+			res.write('Name: ' + fund.name + "; Goal: " + fund.goal + "; Progress: " + fund.progress);
+			res.write(" <a href=\"/edit?name=" + fund.name + "\">[Edit]</a>");
+		}
+		res.end();
+	});
+});
+
+// TODO: edit not working? also send to screen that's like "changes submited!" [redirect to search] [??]
+/**
+ * Modifies a Fund given its name. It gets values from form data and
+ * updates and place back in database. Once done, it sends the user back to the 
+ * view page
+ * author @vagorsol
+*/
+app.use('/modify', (req, res)=> {
+	var filter = req.query.name; 
+	// res.redirect('/public/editfund.html');
+
+	Fund.findOne(filter, (err, fund) =>{
+		console.log("Fund: " + fund);
+		if (err) {
+			res.send('Unexpected Error!');
+		} else if (!fund || fund == null) {
+			// send message that there is no such fund
+			res.send('No such fund exists!');
+		} else {
+			// if the submitted body isn't empty, update the value
+			if (req.body.goal){
+				fund.goal = req.body.goal;
+				console.log("Updated Goal! " + fund.goal);
+			}
+			if (req.body.progress){
+				fund.progress = req.body.progress; 
+				console.log("Updated progress! " + fund.progress);
+			}
+			Fund.findByIdAndUpdate({name: fund.name},{goal: fund.goal}, {progress: fund.progress});
+			
+		}
+	});
+
+	console.log(fund);
+	res.type('html').status(200);
+	res.write('Changes successfully made!');
+	res.write(" <a href=\"/all\">[View All Funds]</a>");
+	res.write(" <a href=\"/request\">[Search for a Fund]</a>");
+
+});
+
+// temporary create function so i can test things
+// endpoint for creating a new person
+// this is the action of the "create new person" form
+app.use('/add', (req, res) => {
+	// construct the Person from the form data which is in the request body
+	var newFund = new Fund ({
+		name: req.body.name,
+		goal: req.body.goal,
+		progress: req.body.progress
+	    });
+	console.log("fund created");
+	// save the person to the database
+	newFund.save().then( (err) => { 
+		res.type('html').status(200);
+		if (!err) {
+		    res.write('uh oh: ' + err);
+		    console.log(err);
+		}
+		else {
+		    // display the "successfull created" message
+		    res.write('Successfully added ' + newFund.name + ' to the database');
+			res.write(" <a href=\"/create\">[Add Another Fund]</a>");
+			res.write(" <a href=\"/request\">[Search for a Fund]</a>");
+		}
+		res.end(); 
+	    } ); 
+    }
+    );
+
+// endpoint for showing all the people
+app.use('/all', (req, res) => {
+    
+	// find all the Person objects in the database
+	Fund.find( {}, (err, funds) => {
+		if (err) {
+		    res.type('html').status(200);
+		    console.log('uh oh' + err);
+		    res.write(err);
+		}
+		else {
+		    if (funds.length == 0) {
+			res.type('html').status(200);
+			res.write('There are no fund');
+			res.end();
+			return;
+		    }
+		    else {
+			res.type('html').status(200);
+			res.write('Here are the people in the database:');
+			res.write('<ul>');
+			// show all the people
+			funds.forEach( (fund) => {
+			    res.write('<li>');
+			    res.write('Name: ' + fund.name + '; Goal: ' + fund.goal + '; Progress: ' + fund.progress);
+			    // this creates a link to the /delete endpoint
+			    res.write(" <a href=\"/edit?name=" + fund.name + "\">[Edit]</a>");
+			    res.write('</li>');
+					 
+			});
+			res.write('</ul>');
+			res.end();
+		    }
+		}
+	    }).sort({ 'name': 'asc' }); // this sorts them BEFORE rendering the results
+});
+
+app.use('/newUser', (req, res) => {
+	// construct the Person from the form data which is in the request body
+	var newUser = new User({
+		username: req.body.username,
+		password: req.body.password,
+		name: req.body.name,
+	    });
+	console.log("user created");
+	// save the person to the database
+	newUser.save().then( (err) => { 
+		res.type('html').status(200);
+		if (!err) {
+		    res.write('uh oh: ' + err);
+		    console.log(err);
+		}
+		else {
+		    // display the "successfull created" message
+		    res.write('Successfully added ' + newUser.name + ' to the database');
+			res.write("<br> <a href=\"/\">Return Home </a>")
+			res.write(" <a href=\"/create\">[Add Fund]</a>");
+			res.write(" <a href=\"/request\">[Search for a Fund]</a>");
+		}
+		res.end(); 
+	    } ); 
+    }
+    );
+/***************************************/
 // This is the '/test' endpoint that you can use to check that this works
 app.use('/test', (req, res) => {
 	// create a JSON object
@@ -9,11 +197,16 @@ app.use('/test', (req, res) => {
 	res.json(data);
     });
 
-// This just sends back a message for any URL path not covered above
-app.use('/', (req, res) => {
-	res.sendFile(__dirname + "/index.html"); });
-
 // This starts the web server on port 3000. 
 app.listen(3000, () => {
 	console.log('Listening on port 3000');
     });
+
+app.use('/public', express.static('public'));
+
+// Directory for linking through html files
+app.get('/', (req, res)=> { res.redirect('/public/index.html'); } );
+app.get('/request', (req, res) => { res.redirect('/public/viewFund.html'); } );
+app.get('/create', (req, res) => {res.redirect('/public/addFund.html'); } ); // rough "add fund" function for testing
+app.get('/edit', (req, res) => {res.redirect('/public/editFund.html'); })
+app.get('/createUser', (req, res) => {res.redirect('/public/newUser.html'); })
