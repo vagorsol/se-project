0ff81bd5257ae9bddd21341cb27ecec1b7f40f52
ghostfish478 <bho@haverford.edu>
2023-04-22 17:19:01 -0400

    Updated with new methods

--- index.js
+++ index.js
@@ -317,9 +317,9 @@ app.use('/modifycontributor', (req, res)=>{
 
 });
 
-// creates a new contributor
+// creates a new Fund
 app.use('/add', (req, res) => {
-	// construct the Person from the form data which is in the request body
+	// construct the Fund from the form data which is in the request body
 	var newFund = new Fund ({
 		name: req.body.name,
 		description: req.body.desscription,
@@ -389,6 +389,47 @@ app.use('/allFunds', (req, res) => {
 		    }
 		}
 	    }) 
+});
+
+// endpoint for sorting funds by progress
+// @author bho
+app.use('/allprogress', (req, res) => { // PLEASE CHECK IF IT NEEDS MODIFICATIONS
+    
+	// find all the Fund objects in the database
+	Person.find( {}, (err, funds) => {
+		if (err) {
+		    res.type('html').status(200);
+		    console.log('uh oh' + err);
+		    res.write(err);
+		}
+		else {
+		    if (persons.length == 0) {
+			res.type('html').status(200);
+			res.write('There are no funds');
+			res.end();
+			return;
+		    }
+		    else {
+			res.type('html').status(200);
+			res.write('Here are the funds in the database:');
+			res.write('<ul>');
+			// show all the people
+			persons.forEach( (fund) => {
+			    res.write('<li>');
+				res.write('Name: ' + fund.name + '; Goal: ' + fund.goal + '; Progress: ' + fund.progress +
+				'; Completion: ' + fund.completion + '% ; Owners: ' + fund.ownerName);
+			    // // this creates a link to the /delete endpoint
+			    res.write(" <a href=\"/delete?name=" + person.name + "\">[Delete]</a>");
+			    res.write('</li>');
+					 
+			});
+			res.write('</ul>');
+			res.end();
+		    }
+		}
+	}).sort(function (x, y) { 
+		return x.completion - y.completion; // completion = fund.progress / (fund.goal * 1.0) * 100
+	});
 });
 
 // endpoint for deleting a fund
@@ -637,13 +678,84 @@ app.use('/contributionHistory', (req, res) => {
 });
 
 // return username function?
-app.use('/getUsernmae', (req, res) => {
+app.use('/getUsername', (req, res) => {
 	if (req.user.username) {
 		res.json({'username' : req.user.username});
 	} else {
 		res.json([]);
 	}
 });
+
+// This is the '/test' endpoint that you can use to check that this works
+app.use('/addNoteForUser', (req, res) => {
+	// create a JSON object
+	var user = {'username': req.query.username};
+	var note = {'note': req.query.note};
+
+	var filter = fundID;  // Bandaid solution for not being able to pass query
+	console.log("Filter: " + filter);
+	// res.redirect('/public/editfund.html');
+	Fund.findOne({'username': filter}).then((fund, err) =>{
+		// console.log("Fund: " + fund);
+		if (err) {
+			res.send('Unexpected Error!');
+		} else if (!fund || fund == null) {
+			console.log(fund);
+			// send message that there is no such fund
+			res.type('html').status(200);
+
+			res.write('No such username exists!');
+			res.write("<p> <a href=\"/allFunds\">[View All Funds]</a>");
+			res.write(" <a href=\"/request\">[Search for a Fund]</a>");
+			res.write("<p> <a href=\"/\">[Return Home]</a>");
+		} else {
+			// if the submitted body isn't empty, update the value
+			if (req.body.note){
+				user.note = req.body.note;
+				user.findByIdAndUpdate({_id: user.id}, {note: user.goal}).then((err, doc) => {
+					if(err){
+						res.write('Unexpected Error!');
+					} else{
+						console.log("Updated note! " + user.note);
+						res.write("<p>Updated + " + user.name + "'s note!");
+					}
+				});
+			}
+
+			res.type('html').status(200);
+			res.write('<p>Changes successfully made to ' + user.name + '!');
+			res.write("<p> <a href=\"/allFunds\">[View All Funds]</a>");
+			res.write(" <a href=\"/request\">[Search for a Fund]</a>");
+			res.write("<p> <a href=\"/\">[Return Home]</a>");
+		}
+		res.end();
+	});
+    // update user
+	Contributor.findOne(username).then((contributor, err) => {
+		var contributionHistory = []
+		if (err) {
+			console.log(err);
+			res.json([]);
+		} else if (!contributor || contributor == null) {
+			// send empty json
+			res.json([]);
+		} else {
+			// if there is no contribution log, create one. otherwise, update
+			if (!contributor.contribution_log || contributor.contribution_log == null) {
+				// send empty json
+				res.json([])
+			} else {
+				contributor.contribution_log.forEach((i) => {
+					contributionHistory.unshift(i); // i don't know what it's actually sending. hm. 
+				});
+				res.send(contributionHistory);
+			}	
+		}
+	})
+	
+	// send it back
+	res.json(data);
+    });
 
 /***************************************/
 // This is the '/test' endpoint that you can use to check that this works
